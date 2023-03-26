104ecf362fb127f6f507e17db79018e959dfdfe4
@Bahatimyk <manenomichael99@gmail.com>
2023-03-26 20:42:41 +0300

    itemlist table

--- app/Http/Controllers/API/itemlistcontroller.php
+++ app/Http/Controllers/API/itemlistcontroller.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
 
 class itemlistcontroller extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        
+    }
 }
