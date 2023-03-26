ccec3fb75f33742549bfe3f78fecd35c078592c6
@Bahatimyk <manenomichael99@gmail.com>
2023-03-26 20:37:23 +0300

    itemlist contoller

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,9 @@ use App\Http\Controllers\API\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
+
+
+
 Route::middleware(['auth:sanctum'])->group(function(){
 
     Route::post('logout', [AuthController::class, 'logout']);
