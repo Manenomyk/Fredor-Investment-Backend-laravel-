3a3fa94699cba1c6971246d276b8618f309ecc9e
@Bahatimyk <manenomichael99@gmail.com>
2023-03-26 21:14:25 +0300

    api code

--- app/Http/Controllers/API/itemlistcontroller.php
+++ app/Http/Controllers/API/itemlistcontroller.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\itemlist;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\FuncCall;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class itemlistcontroller extends Controller
 {
     public function store(Request $request)
     {
-        
+        $validator = Validator::make($request->all(), [
+            'description' => 'Required',
+            'b_price' => 'Required',
+            's_price' => 'Required',
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->massages(),
+            ]);
+        } else {
+            $itemlist = new itemlist;
+            $itemlist->description = $request->input('description');
+            $itemlist->b_price = $request->input('b_price');
+            $itemlist->s_price = $request->input('s_price');
+            $itemlist->save();
+            return response()->json([
+                'status' => 200,
+                'massage' => "Item added successfully",
+            ]);
+        }
     }
 }
--- app/Models/itemlist.php
+++ app/Models/itemlist.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class itemlist extends Model
 {
     use HasFactory;
+    protected $table = 'itemlist';
+    protected $fillable = [
+        'description',
+        'b_price',
+        's_price',
+    ];
 }
