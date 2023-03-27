6ea2f2aa0d6e4fcb033f314755f24c7d9771cf11
@Bahatimyk <manenomichael99@gmail.com>
2023-03-27 09:27:22 +0300

    customerlist controller code

--- app/Http/Controllers/API/customerlistcontoller.php
+++ app/Http/Controllers/API/customerlistcontoller.php
@@ -2,13 +2,42 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\customerlist;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class customerlistcontoller extends Controller
 {
-    public function store(Request $reques)
+    public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'plate_name' => 'required',
+            'plate_code' => 'required',
+            'driver' => 'required',
+            'id_no' => 'required',
+            'phone' => 'required',
+            'company' => 'required',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'validation_errors' => $validator->messages()->all()
+            ], 422);
+        }else {
+            $customerlist = new customerlist;
+            $customerlist->plate_name = $request->input('plate_name');
+            $customerlist->plate_code = $request->input('plate_code');
+            $customerlist->driver = $request->input('driver');
+            $customerlist->id_no = $request->input('id_no');
+            $customerlist->phone = $request->input('phone');
+            $customerlist->company = $request->input('company');
+            $customerlist->save();
+            return response()->json([
+                'status' => 200,
+                'massage' => "customerlist added successfully",
+            ]); 
+        }
     }
+
 }
